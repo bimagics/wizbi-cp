b83cc1403b3e6dc6fffbe219c036292740e13622
OmerCohenBI <omer.cohen@bimagics.com>
2025-09-03 09:51:46 +0300

    Update firebaseAdmin.ts

--- src/services/firebaseAdmin.ts
+++ src/services/firebaseAdmin.ts
@@ -1,17 +1,13 @@
-import { applicationDefault, getApps, initializeApp } from 'firebase-admin/app';
-import { getFirestore } from 'firebase-admin/firestore';
+import { initializeApp, applicationDefault, getApps, getApp } from "firebase-admin/app";
+import { getFirestore, Firestore } from "firebase-admin/firestore";
 
-export function db() {
-  if (!getApps().length) {
-    initializeApp({
-      credential: applicationDefault(),
-      projectId:
-        process.env.FIREBASE_PROJECT_ID ||
-        process.env.GCLOUD_PROJECT ||
-        process.env.GOOGLE_CLOUD_PROJECT,
-    });
-  }
-  const d = getFirestore();
-  d.settings({ ignoreUndefinedProperties: true });
-  return d;
+let app = getApps().length ? getApp() : initializeApp({
+  credential: applicationDefault(),
+  projectId: process.env.FIREBASE_PROJECT_ID || "wizbi-cp",
+});
+
+let db: Firestore = getFirestore(app);
+
+export function getDb() {
+  return db;
 }
